b2898a63a336c74334d464692283186e1059c721
unknown <dommillar@gmail.com>
2013-01-19 23:58:56 +1100

    Added some planets and improved the map display

--- SpaceRogueRevolution/Models/Galaxy.cs
+++ SpaceRogueRevolution/Models/Galaxy.cs
@@ -32,6 +32,20 @@ namespace SpaceRogueRevolution.Models
             //Place the dart
             Tile dart = new Tile { ID = 1, FileName = "dart.png" };
             map[locationPlayer.Row, locationPlayer.Col] = dart;
+
+            //Place planets
+            Random r = new Random();
+            Coord locationPlanet2 = new Coord { Row = r.Next(30), Col = r.Next(30) };
+            Tile planet2 = new Tile { ID = 2, FileName = "planetblue.png" };
+            map[locationPlanet2.Row, locationPlanet2.Col] = planet2;
+
+            Coord locationPlanet3 = new Coord { Row = r.Next(30), Col = r.Next(30) };
+            Tile planet3 = new Tile { ID = 2, FileName = "planetwhite.png" };
+            map[locationPlanet3.Row, locationPlanet3.Col] = planet3;
+
+            Coord locationPlanet = new Coord { Row = r.Next(30), Col = r.Next(30) };
+            Tile planet = new Tile { ID = 2, FileName = "planetbrown.png" };
+            map[locationPlanet.Row, locationPlanet.Col] = planet;
         }
 
 
