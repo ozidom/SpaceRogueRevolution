edc3208310171a10b3c25da5c9dfcc936d50b4f5
unknown <dommillar@gmail.com>
2013-02-16 14:17:13 +1100

    still not deploying

--- SpaceRogueRevolution/Controllers/HomeController.cs
+++ SpaceRogueRevolution/Controllers/HomeController.cs
@@ -35,20 +35,28 @@ namespace SpaceRogueRevolution.Controllers
         [HttpPost]
         public ActionResult Sync(List<Tile> map)
         {
-            Trace.WriteLine("1");
-            Galaxy galaxy = GetGalaxy(map);
-            Trace.WriteLine("2");
-            SetGalaxy(galaxy);
-            if (map != null)
+            Galaxy galaxy;
+            try
             {
-                Trace.WriteLine("3");
-                galaxy.UpdatePlayerStarshipToGameObject(map[0]);
+                Trace.WriteLine("1");
+                galaxy = GetGalaxy(map);
+                Trace.WriteLine("2");
+                SetGalaxy(galaxy);
+                if (map != null && map.Count > 0)
+                {
+                    Trace.WriteLine("3");
+                    galaxy.UpdatePlayerStarshipToGameObject(map[0]);
+                }
+                Trace.WriteLine("4");
+                galaxy.TakeComputerActions();
+                Trace.WriteLine("5");
+                galaxy.UpdateGameObjectsToMap();
+                Trace.WriteLine("6");
             }
-            Trace.WriteLine("4");
-            galaxy.TakeComputerActions();
-            Trace.WriteLine("5");
-            galaxy.UpdateGameObjectsToMap();
-            Trace.WriteLine("6");
+            catch (Exception e)
+            {
+                return (Json("e:" + e.Message));
+            }
             return Json(galaxy.map);
         }
 
